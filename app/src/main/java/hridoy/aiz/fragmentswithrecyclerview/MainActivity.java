493dd167f17dc9d7c7056aee9095055bf67e5f28
hridoymahmud71 <hridoymahmud71@gmail.com>
2019-08-26 13:51:19 +0600

    first commit

---
+++ app/src/main/java/hridoy/aiz/fragmentswithrecyclerview/MainActivity.java
@@ -0,0 +1,13 @@
+package hridoy.aiz.fragmentswithrecyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
